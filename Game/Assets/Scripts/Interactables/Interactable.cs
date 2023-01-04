30ffae99b77ae8a777f86236b594ac342180fbf8
NoahBerhane <113933637+NoahBerhane@users.noreply.github.com>
2023-01-04 22:19:25 +0100

    fick interactable ikonen o funka
    istället för ontriggerenter o exit2d så använde jag raycast (CircleCastAll)

--- Game/Assets/Scripts/Interactables/Interactable.cs
+++ Game/Assets/Scripts/Interactables/Interactable.cs
@@ -3,25 +3,4 @@ using UnityEngine;
 public abstract class Interactable : MonoBehaviour
 {
     public abstract void Interact();
-
-    private void Reset()
-    {
-        GetComponent<BoxCollider>().isTrigger = true;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player")) 
-        {
-            collision.GetComponent<PlayerInteraction>().openInteractableIcon();
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-		if (collision.CompareTag("Player"))
-		{
-			collision.GetComponent<PlayerInteraction>().closeInteractableIcon();
-		}
-	}
 }
